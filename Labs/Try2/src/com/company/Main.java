217b8b9ef51bd856f1ee20df429ac308309b8db2
jtiks06@gmail.com <jtiks06@gmail.com>
2019-03-25 12:24:00 +0200

    Finish first lab, need to add output in table format

---
+++ Labs/Try2/src/com/company/Main.java
@@ -0,0 +1,68 @@
+package com.company;
+
+
+public class Main {
+    static int k = 1;
+
+    static double x_start = -1.83*k + 0.5;
+    static double x_end = 2.38*k + 0.5;
+    static double y_start = -0.74 + 1.0;
+    static double y_end = 0.86*k + 1.0;
+    static double Z = 1.3*k +1.5;
+
+    static int Nx = 10;
+    static int Ny = 15;
+    static int Nz = 1;
+
+    public static void main(String[] args) {
+
+        double [] X = Main.createArray(Main.x_start, Main.x_end, Main.Nx);
+        double [] Y = Main.createArray(Main.y_start, Main.y_end, Main.Ny);
+
+        System.out.print("X= ");
+        Main.outputArray(X);
+        System.out.println();
+        System.out.print("Y= ");
+        Main.outputArray(Y);
+
+        for (int i = 0; i < X.length; ++i){
+            for (int j = 0; j < Y.length; ++j){
+                System.out.println(Main.f2(X[i], Y[i], Main.Z));
+            }
+
+        }
+
+    }
+
+    private static double f1(double x, double y, double z){
+        return (2*z+Math.cos(Math.pow(Math.abs(y-3*x), 1./3)))/(2.1+Math.pow(Math.sin(Math.pow(Math.abs(z*z*z - y), 0.2)), 2))+Math.pow(Math.log(Math.abs(z-x)), 2);
+    }
+
+    private static double f2(double x, double y, double z){
+        return Math.pow(Math.abs(x), 0.43) + (Math.exp(y-x) + Math.sqrt(Math.pow(Math.abs(y*y + Main.f1(x,y,z)) ,0.22)))/(1.72 + x*x*Math.abs(y - Math.tan(z)* Math.tan(z)));
+    }
+
+    private static double[] createArray(double start, double end, int count){
+        // TODO: add checking start < end
+        double step = (end - start) / count;
+        double tempEnd = start;
+        double [] range;
+        range = new double[count];
+
+        int i = 0;
+        while (tempEnd < end && i < count) {
+            range[i] = tempEnd;
+            tempEnd += step;
+            ++i;
+        }
+
+        return range;
+    }
+
+    private static void outputArray(double [] A){
+        for (int i = 0; i < A.length; ++i){
+            System.out.print(A[i]);
+            System.out.print(" ");
+        }
+    }
+}
