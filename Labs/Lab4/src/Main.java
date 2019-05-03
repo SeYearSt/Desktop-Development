0e72ce4a163790fcfee893ef4e35bfa6d3c98c4a
jtiks06@gmail.com <jtiks06@gmail.com>
2019-05-03 14:50:04 +0300

    Final version

--- Labs/Lab4/src/Main.java
+++ Labs/Lab4/src/Main.java
@@ -3,9 +3,9 @@ import classes.Money;
 public class Main {
     public static void main(String [] args){
         float fraction = 3;
-        float d = 4;
-        Money A = new Money(10, 30);
-        Money B = new Money(1, 40);
+        double d = 4;
+        Money A = new Money(100, 10);
+        Money B = new Money(5, 90);
         System.out.print("A=");
         System.out.println(A);
         System.out.print("B=");
@@ -14,9 +14,9 @@ public class Main {
         System.out.println(A.add(B));
         System.out.print("A-B=");
         System.out.println(A.sub(B));
-        System.out.print("A/B=");
+        System.out.print(String.format("A/%f=", fraction));
         System.out.println(A.div(fraction));
-        System.out.print("A*B=");
+        System.out.print(String.format("A*%f=", fraction));
         System.out.println(A.mul(fraction));
         System.out.print("A==B=");
         System.out.println(A.equal(B));
