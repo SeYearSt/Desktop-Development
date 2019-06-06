d25b350def8114a2d6eb80e8e7b62969089d59d7
jtiks06@gmail.com <jtiks06@gmail.com>
2019-06-06 19:37:10 +0300

    Add almost finished lab9

--- Labs/Lab9/src/Main.java
+++ Labs/Lab9/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args){
         MyMatrix a = new MyMatrix();
-        MyMatrix b = new MyMatrix();
+//        MyMatrix b = new MyMatrix();
+        MyMatrix b = MyMatrix.initMatrix(3, 3);
         System.out.println("A");
         System.out.println(a);
         System.out.println("B:");
@@ -12,5 +13,9 @@ public class Main {
         System.out.println(a.sub(b));
         System.out.println("A * B");
         System.out.println(a.mul(b));
+        System.out.println("Catch exception IllegalArgumentException");
+        System.out.println(a.getEl(-1, -1));
+        System.out.println("\nCatch exception IllegalArgumentException");
+        System.out.println(a.getEl(1, 50));
     }
 }
--- Labs/Lab9/src/MyMatrix.java
+++ Labs/Lab9/src/MyMatrix.java
@@ -1,37 +1,97 @@
+import java.util.Scanner;
+
 public class MyMatrix {
-    private int n = 3, m = 3;
+    static private int defaultN = 3, defaultM = 3;
+    private int n = defaultN, m = defaultM;
     private int min = 3, max = 5;
     private double[][] M;
 
     public MyMatrix(){
-        this.M = new double[n][m];
+        this.M = this.initRandomMatrix(this.n, this.m);
+    }
 
-        for(int i=0; i<n; ++i){
-            for(int j=0; j<m; ++j){
-                this.M[i][j] = Math.round((Math.random() * ((max - min) + 1)) + min);
+    public MyMatrix(double[][] M, int n, int m){
+        this.n = n;
+        this.m = m;
+        this.M = M;
+
+        try {
+            if (n <= 0 || m <= 0) {
+                throw new IllegalArgumentException("Matrix's dimensions cannot negative or zero");
             }
+        }catch(IllegalArgumentException exception){
+            System.out.println("Incorrect n or m");
+            System.out.println("Using default values");
+            System.out.println("Creating new random values");
+            this.n = defaultN;
+            this.m = defaultM;
+            this.M = this.initRandomMatrix(n, m);
+
         }
     }
 
-    public MyMatrix(double[][] M){
-        // TODO: check dimensions
-        this.M = M;
-    }
-
     public MyMatrix(int n, int m){
-        // TODO: check numbers
-
+        try {
+            if (n <= 0 || m <= 0) {
+                throw new IllegalArgumentException("Matrix's dimensions cannot negative or zero");
+            }
+        }catch(IllegalArgumentException exception){
+            System.out.println("Incorrect n or m");
+            System.out.println("Using default values");
+            System.out.println("Creating new random values");
+            n = defaultN;
+            m = defaultM;
+        }finally {
+            this.M = this.initRandomMatrix(n, m);
+        }
     }
 
     public static MyMatrix initMatrix(int n, int m){
-        // TODO: check numbers
-        // TODO: check input values
+        try {
+            if (n <= 0 || m <= 0) {
+                throw new IllegalArgumentException("Matrix's dimensions cannot negative or zero");
+            }
+        }catch(IllegalArgumentException exception){
+            System.out.println("Incorrect n or m");
+            System.out.println("Using default values");
+            System.out.println("Creating new random values");
+            n = defaultN;
+            m = defaultM;
+        }
 
-        return null;
+        double[][] M = new double[n][m];
+        Scanner input = new Scanner(System.in);
+        String val;
+
+        System.out.println("Input matrix");
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j) {
+                System.out.print(String.format("M[%d][%d]: ", i + 1, j + 1));
+                val = input.next();
+                try{
+                    M[i][j] = Double.parseDouble(val);
+                }catch (Exception e){
+                    System.out.println("Fail to save value");
+                    System.out.println("Set zezo value");
+                    M[i][j] = 0;
+                }
+            }
+        }
+        return new MyMatrix(M, n, m);
     }
 
     public double[][] getArray(){
         return this.M;
+    }
+
+    public double getEl(int i, int j){
+        try{
+            return this.M[i][j];
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Invalid index");
+            System.out.println("Zero will be returned");
+            return 0;
+        }
     }
 
     public int getDim(int d){
@@ -52,10 +112,11 @@ public class MyMatrix {
                 res[i][j] = aArray[i][j] + this.M[i][j];
             }
         }
-        return new MyMatrix(res);
+        return new MyMatrix(res, n, m);
     }
 
     public MyMatrix sub(MyMatrix a){
+        // TODO: check M dimension
         double[][] res = new double[n][m];
         double[][] aArray = a.getArray();
 
@@ -64,7 +125,7 @@ public class MyMatrix {
                 res[i][j] = this.M[i][j] - aArray[i][j];
             }
         }
-        return new MyMatrix(res);
+        return new MyMatrix(res, n, m);
     }
 
     public MyMatrix mul(MyMatrix a) {
@@ -81,27 +142,15 @@ public class MyMatrix {
             }
         }
 
-        for (int i = 0; i < n; i++) { // aRow
-            for (int j = 0; j < m; j++) { // bColumn
-                for (int k = 0; k < this.m; k++) { // aColumn
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < this.m; k++) {
                     res[i][j] += this.M[i][k] * aMatrix[k][j];
                 }
             }
-
         }
-        return new MyMatrix(res);
+        return new MyMatrix(res, n, m);
     }
-
-//    public static void outputMatrix(int[][] M){
-//        for (int i = 0; i < M.length; i++) {
-//            for (int j = 0; j < M[i].length; j++) {
-//                System.out.print(String.format("M[%d][%d]: ", i+1, j+1) + M[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
-//    }
-
-    // TODO: add check if number is negative
 
     @Override
     public String toString() {
@@ -114,5 +163,15 @@ public class MyMatrix {
             resultString += "\n";
         }
         return resultString;
+    }
+    private double[][] initRandomMatrix(int n, int m){
+        double[][] M = new double[n][m];
+
+        for(int i=0; i<n; ++i){
+            for(int j=0; j<m; ++j){
+                M[i][j] = Math.round((Math.random() * ((max - min) + 1)) + min);
+            }
+        }
+        return M;
     }
 }
